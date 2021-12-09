15f05d0025b5cae26360936f8bf3217bc40919a9
eb <eric.boniardi@mail.polimi.it>
2021-12-09 16:50:13 +0100

    cleaning

--- root/controller/cavi/updater/update_test.py
+++ root/controller/cavi/updater/update_test.py
@@ -4,4 +4,6 @@ from model.variational_parameters import VariationalParameters
 
 
 def update_parameters(data, hyperparameters: HyperparametersModel, variational_parameters: VariationalParameters):
-    variational_parameters.nIW_MIX_VAR.
+    variational_parameters.nIW_MIX_VAR.mu
+
+    hyperparameters.nIW_MIX_0.mu
