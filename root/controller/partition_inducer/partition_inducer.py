47d14116d13b487923ee615d29e62f3d7b0a092c
eb <eric.boniardi@mail.polimi.it>
2021-12-16 13:54:09 +0100

    specify_user_input_our.py

--- root/controller/partition_inducer/partition_inducer.py
+++ root/controller/partition_inducer/partition_inducer.py
@@ -8,7 +8,7 @@ def generate_induced_partition(Y, robust_mean, variational_parameters : Variatio
     for i in range(750):
         ll.append(jnp.argmax(variational_parameters.phi_m_k[i, :]))
     plt.scatter(Y[:, 0], Y[:, 1], c=ll, s=40, cmap='viridis')
-    plt.scatter(robust_mean[:, 0], robust_mean[:, 1], c='red', s=40, cmap='viridis')
+    #plt.scatter(robust_mean[:, 0], robust_mean[:, 1], c='red', s=40, cmap='viridis')
     # plt.show()
     plt.savefig('figure.png')
     print("\n\nPLOT available in /content/VariationalBRAND/tests/figure.png")
