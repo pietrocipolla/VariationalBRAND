7c1b84a7e6c4d004e8a347cd0c8703c91548c198
Eric <eric.boniardi@mail.polimi.it>
2021-12-28 09:25:09 +0100

    tests on not_small_brand.csv

--- root/controller/sample_data_handler/test_init.py
+++ root/controller/sample_data_handler/test_init.py
@@ -26,7 +26,7 @@ def test_init(mu_0_DP, PHI_0_DP):
 
 def test_mu_var_DP_init():
     p = 2
-    T = 30
+    T = 10
     mu_0_DP = np.ones(p)
     #mu_0_DP = np.zeros(p)
     PHI_0_DP = np.multiply(np.identity(p), 10)
